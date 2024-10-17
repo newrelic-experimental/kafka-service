2e0d744814676ce0d27c4b7f2537596403109b1b
kmcfarland <kmcfarland@newrelic.com>
2024-10-17 09:37:24 -0700

    Placeholder code (#6)
    * Placeholder code
    
    * Spotless

--- src/main/kotlin/com/newrelic/demokafkaservice/Processor.kt
+++ src/main/kotlin/com/newrelic/demokafkaservice/Processor.kt
@@ -1,7 +1,5 @@
 package com.newrelic.demokafkaservice
 
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import com.newrelic.api.agent.NewRelic
 import com.newrelic.autoservices.AbstractScheduledAutoService
 import com.newrelic.autoservices.AutoServices
 import com.newrelic.demokafkaservice.config.ProcessorConfig
@@ -11,7 +9,6 @@ import org.slf4j.LoggerFactory
 import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 import javax.inject.Singleton
-import kotlin.random.Random
 
 @Singleton
 class Processor
@@ -30,29 +27,11 @@ class Processor
     override fun runOneIteration() {
         monitoringRecorder.incrementCounter("demo-kafka-service.iterations.ran", 1)
 
-        val messagesToSend = Random.nextInt(1000, 10000)
-        val messagePayloads = generatePayloads(messagesToSend)
-
-        logger.info("Sending $messagesToSend events to kafka")
-        messagePayloads.forEach { payload ->
-            try {
-                if (config.explodeLogCount) logger.info("sending event with attributes: ${payload.keys}")
-
-                val serializedPayload = jacksonObjectMapper().writeValueAsBytes(payload)
-                kafkaProducer.sendEvent(serializedPayload)
-            } catch (ex: Exception) {
-                if (config.noticeError) NewRelic.noticeError(ex)
-
-                if (config.logError) logger.error("Failed to send kafka event: ${ex.message}", ex)
-            }
-        }
-
-        if (config.explodeMemory) messagesSent.addAll(messagePayloads)
+        // TODO: Build out kafka publishing logic
 
         monitoringRecorder.incrementCounter(
             "demo-kafka-service.iterations.completed",
-            1,
-            mapOf("messagesSent" to messagesToSend)
+            1
         )
         logger.info("Finished iteration")
     }
